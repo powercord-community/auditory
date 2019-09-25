ed885e1e3703e3d588b381aab68df3e7648b7996
Blake <bsward8@gmail.com>
2019-09-25 20:13:53 +1000

    fix(Settings.jsx): sliders not rendering correctly

--- Settings.jsx
+++ Settings.jsx
@@ -4,25 +4,24 @@ const { TextInput, SwitchItem } = require('powercord/components/settings');
 module.exports = class Settings extends React.Component {
   constructor (props) {
     super(props);
-
-    const get = props.settings.get.bind(props.settings);
+    this.props = props;
     this.plugin = powercord.pluginManager.get('auditory');
-    this.state = {
+    this.state = {};
+  }
+
+  async componentDidMount () {
+    const get = this.props.settings.get.bind(this.props.settings);
+    this.setState({
+      Slider: await getModuleByDisplayName('Slider'),
+      RadioGroup: await getModuleByDisplayName('RadioGroup'),
+      FormTitle: await getModuleByDisplayName('FormTitle'),
+      FormText: await getModuleByDisplayName('FormText'),
       beastiness: get('beastiness', 1),
       mode: get('mode', 'amp'),
       color: get('color', null),
       brightness: get('brightness', 1),
       important: get('important', false),
       defaultcolor: get('defaultcolor', '')
-    };
-  }
-
-  async componentDidMount () {
-    this.setState({
-      Slider: await getModuleByDisplayName('Slider'),
-      RadioGroup: await getModuleByDisplayName('RadioGroup'),
-      FormTitle: await getModuleByDisplayName('FormTitle'),
-      FormText: await getModuleByDisplayName('FormText')
     });
   }
 
@@ -39,12 +38,12 @@ module.exports = class Settings extends React.Component {
         </FormText>
         <Slider
           defaultValue={this.state.beastiness || 1}
-          handleSize={10}
+          initialValue={this.state.beastiness || 1}
           style={{ marginTop: '32px',
             marginBottom: '32px' }}
           markers={[ 1, 2, 3, 4, 5 ]}
+          handleSize={10}
           stickToMarkers={true}
-          equidistance={true}
           minValue={1}
           maxValue={5}
           onValueChange={(e) => this._set('beastiness', e)}
@@ -53,10 +52,10 @@ module.exports = class Settings extends React.Component {
         <FormTitle style={{ marginTop: '32px' }}>Brightness</FormTitle>
         <Slider
           defaultValue={this.state.brightness || 1}
+          initialValue={this.state.brightness || 1}
           handleSize={10}
           style={{ marginTop: '16px' }}
           stickToMarkers={false}
-          equidistance={true}
           minValue={1}
           maxValue={100}
           onValueChange={(e) => this._set('brightness', e)}
